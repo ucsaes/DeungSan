d9ca6f7bd983fc777ed8b06c6337f1dc463cda45
김민준 <kimminjun0215@gmail.com>
2025-07-04 18:36:44 +0900

    feat: local storage imigration implementation

--- app/src/main/java/com/example/deungsan/data/loader/JsonLoader.kt
+++ app/src/main/java/com/example/deungsan/data/loader/JsonLoader.kt
@@ -7,17 +7,18 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.example.deungsan.data.model.Mountain
 import com.example.deungsan.data.model.Review
+import java.io.File
 
 
 object JsonLoader {
     fun loadMountainsFromAssets(context: Context): List<Mountain> {
-        val json = context.assets.open("mountain.json").bufferedReader().use { it.readText() }
+        val json = File(context.filesDir,"mountain.json").readText()
         val gson = Gson()
         val type = object : TypeToken<List<Mountain>>() {}.type
         return gson.fromJson(json, type)
     }
     fun loadReviewsFromAssets(context: Context): List<Review> {
-        val json = context.assets.open("reviews.json").bufferedReader().use { it.readText() }
+        val json = File(context.filesDir,"reviews.json").readText()
         val gson = Gson()
         val type = object : TypeToken<List<Review>>() {}.type
         return gson.fromJson(json, type)
