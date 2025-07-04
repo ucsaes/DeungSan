e875b3b5e8e61bf696e8da2274af200d820d9360
김민준 <kimminjun0215@gmail.com>
2025-07-04 11:13:26 +0900

    main

--- app/src/main/java/com/example/deungsan/data/DataLoader.kt
+++ app/src/main/java/com/example/deungsan/data/DataLoader.kt
@@ -2,15 +2,3 @@ package com.example.deungsan.data
 
 import java.io.IOException
 
-fun getJsonDataFromAsset(): String? {
-    val jsonString: String
-    try {
-        jsonString = requireActivity().assets.open("communityData.json")
-            .bufferReader()
-            .use { it.readText() }
-    }catch (ioException: IOException) {
-        ioException.printStackTrace()
-        return null
-    }
-    return jsonString
-}
